c8d12271823b0ad0cae09a4b4390873ab661541d
x123 <x123@users.noreply.github.com>
2024-11-30 19:56:02 +0100

    2022: day2-2

--- 2022/cmd/day2.go
+++ 2022/cmd/day2.go
@@ -5,15 +5,12 @@ package cmd
 
 import (
 	"fmt"
-	// "log"
-	// "slices"
-	// "sort"
-	// "strconv"
 	"strings"
 )
 
 var (
-	moveMap = make(map[byte]int)
+	moveMap     = make(map[byte]int)
+	strategyMap = make(map[byte]string)
 )
 
 func day2_1(content string) {
@@ -25,11 +22,10 @@ func day2_1(content string) {
 	fmt.Printf("content:\n%s\n", content)
 
 	var totalPoints int
-	for i, line := range strings.Split(content, "\n") {
-		fmt.Printf("i:%d,line:'%s'\n", i, line)
-		moves := strings.Split(line, " ")
-		fmt.Printf("moves:%s\n", moves)
+
+	for _, line := range strings.Split(content, "\n") {
 		if line != "" {
+			moves := strings.Split(line, " ")
 			opponentMove, myMove := moves[0][0], moves[1][0] // offset of 23
 			fmt.Printf("opponentMove:%c(%d),myMove:%c(%d)\n", opponentMove, int(opponentMove), myMove, int(myMove))
 
@@ -57,4 +53,54 @@ func day2_1(content string) {
 func day2_2(content string) {
 	fmt.Printf("day:%d part:%d\n", day, part)
 	fmt.Printf("content:%s\n", content)
+
+	moveMap['A'] = 1 // rock
+	moveMap['B'] = 2 // paper
+	moveMap['C'] = 3 // scissors
+	moveMap['X'] = 1 // rock
+	moveMap['Y'] = 2 // paper
+	moveMap['Z'] = 3 // scissors
+
+	strategyMap['X'] = "lose"
+	strategyMap['Y'] = "draw"
+	strategyMap['Z'] = "win"
+
+	var totalPoints int
+	for _, line := range strings.Split(content, "\n") {
+		if line != "" {
+			moves := strings.Split(line, " ")
+			opponentMove, strategy := moves[0][0], moves[1][0] // offset of 23
+			fmt.Printf("opponentMove:%c(%d),strategy:%c(%s)\n", opponentMove, int(opponentMove), strategy, strategyMap[strategy])
+			if strategyMap[strategy] == "draw" {
+				points := moveMap[opponentMove] + 3
+				totalPoints += points
+				fmt.Printf("opponentMove:%c(%d),strategy:%c(%s) points:%d\n", opponentMove, int(opponentMove), strategy, strategyMap[strategy], points)
+			} else if strategyMap[strategy] == "lose" {
+				var myMove byte
+				if opponentMove == 'A' {
+					myMove = 'C'
+				} else if opponentMove == 'B' {
+					myMove = 'A'
+				} else if opponentMove == 'C' {
+					myMove = 'B'
+				}
+				points := moveMap[myMove]
+				totalPoints += points
+				fmt.Printf("opponentMove:%c(%d),strategy:%c(%s) points:%d\n", opponentMove, int(opponentMove), strategy, strategyMap[strategy], points)
+			} else if strategyMap[strategy] == "win" {
+				var myMove byte
+				if opponentMove == 'A' {
+					myMove = 'B'
+				} else if opponentMove == 'B' {
+					myMove = 'C'
+				} else if opponentMove == 'C' {
+					myMove = 'A'
+				}
+				points := moveMap[myMove] + 6
+				totalPoints += points
+				fmt.Printf("opponentMove:%c(%d),strategy:%c(%s) points:%d\n", opponentMove, int(opponentMove), strategy, strategyMap[strategy], points)
+			}
+		}
+	}
+	fmt.Printf("totalPoints:%d\n", totalPoints)
 }
