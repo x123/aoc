44acf4216ba330635800f0d01b9ba5d13e75387d
x123 <x123@users.noreply.github.com>
2024-12-03 09:11:49 +0100

    2024: day3-2

--- 2024/cmd/day3.go
+++ 2024/cmd/day3.go
@@ -36,8 +36,34 @@ func day3_1(content string) {
 
 func day3_2(content string) {
 	fmt.Printf("day:%d,part:%d\n", day, part)
-	length := len(strings.Split(content, "\n"))
-	for _, line := range strings.Split(content, "\n")[:length-1] {
-		fmt.Printf("%s\n", line)
+	var totalResult int
+
+	re := regexp.MustCompile(`(?P<operator>do\(\)|don\'t\(\)|mul\((?P<x>[0-9]{1,3}),(?P<y>[0-9]{1,3})\))`)
+	matches := re.FindAllStringSubmatch(content, -1)
+
+	do := true // first slice will always be a do()
+	for _, match := range matches {
+		operator := match[re.SubexpIndex("operator")]
+		if operator == "don't()" {
+			do = false
+		} else if operator == "do()" {
+			do = true
+		}
+
+		if do && strings.HasPrefix(operator, "mul(") {
+			x, err := strconv.Atoi(match[re.SubexpIndex("x")])
+			if err != nil {
+				log.Fatal(err)
+			}
+			y, err := strconv.Atoi(match[re.SubexpIndex("y")])
+			if err != nil {
+				log.Fatal(err)
+			}
+			fmt.Printf("match.operator:%v\n", match[re.SubexpIndex("operator")])
+			fmt.Printf("match.x:%v\n", x)
+			fmt.Printf("match.y:%v\n", y)
+			totalResult += (x * y)
+		}
 	}
+	fmt.Printf("%d\n", totalResult)
 }
