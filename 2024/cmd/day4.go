a0e12216c9ff2ae1cc00eea24dc2597fe27e246e
x123 <x123@users.noreply.github.com>
2024-12-05 11:09:37 +0000

    2024: day4-2

--- 2024/cmd/day4.go
+++ 2024/cmd/day4.go
@@ -11,19 +11,9 @@ import (
 	"strings"
 )
 
-func day4_1(content string) {
-	fmt.Printf("day:%d,part:%d\n", day, part)
-
-	var linesParsed int
-
-	// print lines
-	length := len(strings.Split(content, "\n"))
-	for _, line := range strings.Split(content, "\n")[:length-1] {
-		fmt.Printf("%s\n", line)
-		linesParsed++
-	}
-
+func day4LoadMatrix(content string) [][]rune {
 	// load matrix
+	length := len(strings.Split(content, "\n"))
 	xDim := len(strings.Split(content, "\n")[0])
 	yDim := length - 1
 	matrix := make([][]rune, yDim)
@@ -37,20 +27,32 @@ func day4_1(content string) {
 		}
 		matrix[i] = runeArray
 	}
-
 	fmt.Printf("xDim:%d,xYdim:%d\n", xDim, yDim)
-	// fmt.Printf("matrix:%c\n", matrix)
 	for _, row := range matrix {
 		fmt.Printf("%c\n", row)
 	}
-	// result := day4FindChars(matrix, 'X')
-	// for i := range result {
-	// 	fmt.Printf("result:%v\n", result[i])
-	// }
-	day4FindChars(matrix)
+	return matrix
 }
 
-func day4FindChars(matrix [][]rune) {
+func day4_1(content string) {
+	fmt.Printf("day:%d,part:%d\n", day, part)
+
+	var linesParsed int
+
+	// print lines
+	length := len(strings.Split(content, "\n"))
+	for _, line := range strings.Split(content, "\n")[:length-1] {
+		fmt.Printf("%s\n", line)
+		linesParsed++
+	}
+
+	// load matrix
+	matrix := day4LoadMatrix(content)
+
+	day4FindXMAS(matrix)
+}
+
+func day4FindXMAS(matrix [][]rune) {
 	fmt.Printf("len(matrix):%d\n", len(matrix))
 	var totalFound int
 	found := make([][]bool, len(matrix))
@@ -143,15 +145,65 @@ func day4FindChars(matrix [][]rune) {
 	fmt.Printf("totalFound:%d\n", totalFound)
 }
 
-func day4_2(content string) {
-	fmt.Printf("day:%d,part:%d\n", day, part)
-	length := len(strings.Split(content, "\n"))
-	var totalSafe int
-	for _, line := range strings.Split(content, "\n")[:length-1] {
-		ints := day2SplitLine(line)
-		fmt.Printf("%d safe:%t\n", ints, day2IsSafe(ints))
-		if day2IsSafe(ints) {
-			totalSafe += 1
+func day4FindMAS(matrix [][]rune) {
+	fmt.Printf("len(matrix):%d\n", len(matrix))
+	var totalFound int
+	found := make([][]bool, len(matrix))
+	for i := range found {
+		found[i] = make([]bool, len(matrix[0]))
+	}
+	for y, line := range matrix {
+		for x := range line {
+			runeChar := matrix[y][x]
+
+			if runeChar == 'A' {
+				if y+1 <= len(matrix)-1 && y-1 >= 0 {
+					if x+1 <= len(matrix[0])-1 && x-1 >= 0 {
+						var diagRight []rune = []rune{matrix[y-1][x+1]}
+						diagRight = append(diagRight, matrix[y][x])
+						diagRight = append(diagRight, matrix[y+1][x-1])
+
+						var diagLeft []rune = []rune{matrix[y-1][x-1]}
+						diagLeft = append(diagLeft, matrix[y][x])
+						diagLeft = append(diagLeft, matrix[y+1][x+1])
+
+						strDiagRight := string(diagRight)
+						strDiagLeft := string(diagLeft)
+						if (strDiagRight == "MAS" || strDiagRight == "SAM") && (strDiagLeft == "MAS" || strDiagLeft == "SAM") {
+							found[y-1][x-1] = true
+							found[y-1][x+1] = true
+							found[y][x] = true
+							found[y+1][x-1] = true
+							found[y+1][x+1] = true
+
+							// fmt.Printf("strDiagRight:%s\n", strDiagRight)
+							// fmt.Printf("strDiagLeft:%s\n", strDiagLeft)
+							totalFound++
+						}
+
+					}
+				}
+			}
 		}
 	}
+	// pretty print matrix
+	for y := range len(matrix) {
+		for x := range len(matrix[y]) {
+			if found[y][x] {
+				fmt.Printf("%c", matrix[y][x])
+			} else {
+				fmt.Printf(".")
+			}
+		}
+		fmt.Printf("\n")
+	}
+	fmt.Printf("totalFound:%d\n", totalFound)
+}
+
+func day4_2(content string) {
+	fmt.Printf("day:%d,part:%d\n", day, part)
+	// load matrix
+	matrix := day4LoadMatrix(content)
+
+	day4FindMAS(matrix)
 }
